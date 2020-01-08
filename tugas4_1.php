8dfafcd6437f956c5b5c53bd3c90ab4526bb28af
Muhammad Ridwan R <jongsky_1821@yahoo.com>
2020-01-08 19:59:51 +0700

    Tugas 4_1 PHP
    Pengumpulan Tugas untuk Niomic

---
+++ tugas4_1.php
@@ -0,0 +1,12 @@
+<?php
+echo "Harga Beli = Rp 8000 <br>";
+echo "Harga Jual = Rp 7500 <br>";
+echo "Keuntungan = Rp 300 <br>";
+echo "Berapa Jumlah Kambing yang dibeli ? <br>";
+echo "Jawaban : ";
+
+$x = 150000;
+$y = 12.5;
+
+$z = $x + ($x * $y / 100);
+echo "Rp $z";
