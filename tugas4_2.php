fd0d7b0f015636ef8747992c54b4726d196baf89
Muhammad Ridwan R <jongsky_1821@yahoo.com>
2020-01-08 20:00:10 +0700

    Tugas 4_2 PHP
    Pengumpulan Tugas untuk Niomic

---
+++ tugas4_2.php
@@ -0,0 +1,11 @@
+<?php
+echo "Jumlah Saluran = 523 <br>";
+echo "Jumlah terpakai = 8891 <br>";
+echo "Rata-rata Pemakaian = ...? <br>";
+echo "Jawaban : ";
+
+$x = 523;
+$y = 8891;
+
+$z = $y / $x;
+echo "$z liter/keluarga";
